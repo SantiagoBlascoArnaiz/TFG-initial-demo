d5c3719d2338e368557fc6183ba083ad738aa70f
Santiago Blasco Arnaiz <santymosby@gmail.com>
2020-09-24 10:38:11 +0200

    Update ASRActivity.java
    Change message

--- app/src/main/java/com/example/tfg_initial_demo/ASRActivity.java
+++ app/src/main/java/com/example/tfg_initial_demo/ASRActivity.java
@@ -61,7 +61,7 @@ public class ASRActivity extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Di algo ahi a ver");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Di algo");
 
         try {
 
